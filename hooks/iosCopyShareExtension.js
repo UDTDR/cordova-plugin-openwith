6ae61927387430ac7a7c31f77324f088d376b0b0
Philipp Beck <dertieran@gmail.com>
2019-01-29 16:30:05 +0100

    :construction: Work on iOS plugin

--- hooks/iosCopyShareExtension.js
+++ hooks/iosCopyShareExtension.js
@@ -1,37 +1,6 @@
-//
-//  iosCopyShareExtension.js
-//  This hook runs for the iOS platform when the plugin or platform is added.
-//
-// Source: https://github.com/DavidStrausz/cordova-plugin-today-widget
-//
-
-//
-// The MIT License (MIT)
-//
-// Copyright (c) 2017 DavidStrausz
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-//
-
 var fs = require('fs');
 var path = require('path');
-const PLUGIN_ID = "com.zenkit.cordova.openwith";
+const PLUGIN_ID = "cordova-plugin-openwith";
 
 function redError(message) {
     return new Error('"' + PLUGIN_ID + '" \x1b[1m\x1b[31m' + message + '\x1b[0m');
--- hooks/iosRemoveTarget.js
+++ hooks/iosRemoveTarget.js
@@ -29,7 +29,7 @@
 // THE SOFTWARE.
 //
 
-const PLUGIN_ID = "com.zenkit.cordova.openwith";
+const PLUGIN_ID = "cordova-plugin-openwith";
 const BUNDLE_SUFFIX = ".shareextension";
 
 var fs = require('fs');
